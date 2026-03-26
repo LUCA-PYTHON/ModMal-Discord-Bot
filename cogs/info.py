8bffa88f380bca612899b405d468bb860af1bc23
Luca <87448287+LUCA-PYTHON@users.noreply.github.com>
2026-03-26 16:37:57 +0100

    Modmail Discord Bot
    Co-authored-by: Max1385 <robertnehmer69@gmail.com>

--- cogs/info.py
+++ cogs/info.py
@@ -6,6 +6,7 @@ from discord import app_commands
 from discord.app_commands import Choice
 from discord.ext import commands
 
+
 class Button_Fuehrung(discord.ui.View):
     def __init__(self, author_id):
         super().__init__()
@@ -784,6 +785,7 @@ class info(commands.Cog):
 
                 if db_daten is None:
                     await interaction.response.send_message("Nicht gebannt.")
+
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(info(bot))
